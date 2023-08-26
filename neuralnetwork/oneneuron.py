4f1dd0484691f379d09d299a5b6d8ba7343d964c
ErickBezerrar <erickbrtrindade@gmail.com>
2023-08-26 08:12:46 -0300

    network learning

--- neuralnetwork/oneneuron.py
+++ neuralnetwork/oneneuron.py
@@ -1,9 +1,11 @@
+import math 
+
 input = 1
 output_desire = 0
 input_weight = 0.5
-sum = (input * input_weight)
 
- 
+learning_rate = 0.01
+
 def activation(sum):
     if sum >= 0:
         return 1
@@ -11,6 +13,18 @@ def activation(sum):
         return 0
 
 print("entrada", input, "desejado", output_desire)
-output = activation(sum)
-print("saida", output)
-error = output_desire - output
+
+error = math.inf
+
+
+while not error  == 0:
+    sum = input * input_weight
+    output = activation(sum)
+    print("saida", output)
+    error = output_desire - output
+    print("err0", error)
+
+
+    if not error ==  0:
+        input_weight = input_weight + (learning_rate * input * error)
+print("Se entrou aqui a rede aprendeu")
